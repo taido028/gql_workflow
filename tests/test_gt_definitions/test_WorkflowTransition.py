ffdd4c9b44b53413e9df761fe09994ee1c2f54e7
Getbricked <buiminhquang2812@gmail.com>
2024-02-15 00:18:56 +0100

    Final commit ! (Fixed small issues)

--- tests/test_gt_definitions/test_WorkflowTransition.py
+++ tests/test_gt_definitions/test_WorkflowTransition.py
@@ -85,6 +85,6 @@ test_delete_workflowtransition = createFrontendQuery(
             }
         }
     }""",
-    variables={"workflowId": "22b0aec9-a0d4-4a8b-a34d-6481fe45f14b", "sourcestateId": "eb46ece6-be1b-4142-a5c5-0aac31e681f0", "destinationstateId": "31b95aba-4de0-43f5-91ec-ba69183b1ffe"},
+    variables={"workflowId": "8299eeeb-99e7-4364-8cc2-88b83f900d32", "sourcestateId": "eb46ece6-be1b-4142-a5c5-0aac31e681f0", "destinationstateId": "31b95aba-4de0-43f5-91ec-ba69183b1ffe"},
     
 )
